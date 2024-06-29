87a3377770a6b5f0d56b524cde34f89992cb18b1
ronipereira <ronicp@hotmai.com>
2024-06-29 18:36:37 -0300

    include cypress.env.json

--- cypress/e2e/api-mensagen.cy.js
+++ cypress/e2e/api-mensagen.cy.js
@@ -1,15 +1,13 @@
 describe('Api adopet', () =>{ 
-    const authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzMjlkNDE0YS0yZDdjLTQ4NjgtYjY4OC00NDJlYTM3YTQ1MjYiLCJhZG9wdGVyTmFtZSI6Ikpvc8OpIE1pZ3XDqSIsImlhdCI6MTcxODQ4NzA3OCwiZXhwIjoxNzE4NzQ2Mjc4fQ.X3e7ACFL-HRspqi6WN_M-lOfM8n-Zvo34sTatTjmrVM'
-
     it('Mensagens da API', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/329d414a-2d7c-4868-b688-442ea37a4526',
-            headers: { authorization }
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
             expect(res.body).to.have.property('msg')
         })
     })
-})
+}) 
